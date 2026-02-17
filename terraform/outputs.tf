f5e179aa310ce756fff46ed799390cac6d607a85
unix_anand <unixanand2005@gmail.com>
2026-02-17 14:20:31 +0000

    Moving the modified ansible

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,6 +5,6 @@ output "Jenkins_Server_Public_Ip" {
   value = aws_instance.jenkins_server.public_ip
 }
 
-resource "aws_s3_bucket" "kops_state" {
-  bucket = "devops-proj1-kops-state-bucket-unique"
+output "kops_state_bucket" {
+  value = aws_s3_bucket.kops_state.id
 }
