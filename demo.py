9b29d63575379e4c73809e55c36dc68714835bee
Anand <unixanand2005@gmail.com>
2026-02-21 06:02:34 +0530

    Moving updated Jenkinsfile

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import streamlit as st
-st.set_page_config(page_title="My K8s Demo", layout="wide")
-st.title("Streamlit on K8s POD")
-st.success("Version 2.0 - Deployed in Kubenetes POD")
+st.set_page_config(page_title="My K3s Demo", layout="wide")
+st.title("Streamlit on K3s POD")
+st.success("Version 3.0 - Deployed in Kubenetes POD")
 st.sidebar.selectbox("Menu", ["Home", "Dashboard", "About"])
 st.balloons()
 st.sidebar.markdown("---")
